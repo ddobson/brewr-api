e63e43016df5b1ae2a00100a788fea2a8016226b
ddobson <ddobson445@gmail.com>
2017-02-22 07:26:05 -0500

    Add more robust json for update script
    Includes json with ingredient attributes

--- scripts/recipes/update.sh
+++ scripts/recipes/update.sh
@@ -7,8 +7,31 @@ curl "${API}${URL_PATH}" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "recipe": {
-      "final_gravity": 1.150,
-      "abv": '"${ABV}"'
+      "name": "'"${NAME}"'",
+      "instructions": "do stuff",
+      "recipe_ingredients_attributes": [
+        {
+          "quantity": 5.0,
+          "ingredient_attributes": {
+            "name": "oatmeal",
+            "unit": "ounce"
+          }
+        },
+        {
+          "quantity": 6.0,
+          "ingredient_attributes": {
+            "name": "super malt extract",
+            "unit": "ounce"
+          }
+        }
+      ],
+      "summary": "A fucking beer",
+      "original_gravity": 1.013,
+      "final_gravity": 1.104,
+      "abv": 5.4,
+      "ibu": 50,
+      "srm": 17,
+      "image_url": "http://i.imgur.com/PKDP9QO.jpg"
     }
   }'
 
